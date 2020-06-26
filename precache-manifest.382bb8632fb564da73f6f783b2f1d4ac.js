28201517ee151fb9b2da0a20cc2758dba98426dd
NhanMai <mvtnhan@gmail.cpm>
2020-06-26 23:08:53 +0700

    Updates

---
+++ precache-manifest.382bb8632fb564da73f6f783b2f1d4ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896237c66c70533bc2b66ab419b3e541",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52847594f34166b64309",
+    "url": "./static/js/2.32bc41e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.32bc41e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c554e6ba7ab66a91d307",
+    "url": "./static/js/main.2203c369.chunk.js"
+  },
+  {
+    "revision": "94f469ce65b6e46f81eb",
+    "url": "./static/js/runtime-main.df600408.js"
+  },
+  {
+    "revision": "f97dc14b469e7aba06579207a33b1946",
+    "url": "./static/media/bg.f97dc14b.png"
+  },
+  {
+    "revision": "cab12c378520d317a8ec94585fa4af5f",
+    "url": "./static/media/bird.cab12c37.png"
+  },
+  {
+    "revision": "6ecbd5373509a49868bb5c373f34d881",
+    "url": "./static/media/fg.6ecbd537.png"
+  },
+  {
+    "revision": "f791429212203924c69e2f4096681cea",
+    "url": "./static/media/pipe-bottom.f7914292.png"
+  },
+  {
+    "revision": "07cd2335e8acb92c775ef37c15a9016c",
+    "url": "./static/media/pipe-top.07cd2335.png"
+  }
+]);
